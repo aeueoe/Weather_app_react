f8a74aaac90d23a08bfd6487302b2afadc9cb012
aeueoe <drom.jul@gmail.com>
2023-12-10 10:48:20 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
 import ScaleLoader from "react-spinners/ScaleLoader";
